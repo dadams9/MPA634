59dce05e1de7f6ee932ea42080211b9012682e03
Devin Adams <devin_adams3@hotmail.com>
2019-01-10 16:23:07 -0700

    Start of Hw 2, finished 1-3

---
+++ HW_2.R
@@ -0,0 +1,47 @@
+# Introductory Script
+# Initial: January 7, 2019
+# Revision: January 7, 2019
+# Ray Nelson
+# Homework Revisions made by Devin Adams, January 10, 2019
+
+# Libraries
+library(tidyverse)
+
+# mpg data set
+data(mpg)
+?mpg
+glimpse(mpg)
+
+# Simple scatterplot of mileage on displacement
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point()
+
+# Add a loess line
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth()
+
+# Add a linear regression line
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth(color = "red") +
+  geom_smooth(method = "lm")
+
+# Looking at the data by class of car
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = class)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+#Map a continuous variable to color
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = cty)) + 
+  geom_point()
+
+#Map a continuous variable to size
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, size= cty, color = cty)) + 
+  geom_point()
+
+#Map a continuous variable to shape
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, shape = displ)) + 
+  geom_point()
+
